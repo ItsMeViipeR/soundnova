20a84fedcf884639d625ad85b1aae300c0f1f04b
Nicolas AMATI <skkyz3r.dev@gmail.com>
2024-01-05 20:40:12 +0100

    fix: :bug: Fix onTap type to match it with Flutter's onTap

--- lib/appbar.dart
+++ lib/appbar.dart
@@ -2,20 +2,20 @@ import 'package:flutter/material.dart';
 
 TextStyle _textStyle = const TextStyle(color: Colors.white);
 
-Widget _menuItem(String title, Function onTap) {
+Widget _menuItem(String title, void Function()? onTap) {
   return ListTile(
     title: Text(title, style: _textStyle),
-    onTap: () => onTap,
+    onTap: onTap,
   );
 }
 
 Drawer appbar(String appTitle) {
   return Drawer(
       backgroundColor: Colors.blueGrey,
-      child: Align(
-          alignment: Alignment.bottomCenter,
-          child: ListView(
-            children: <Widget>[
+      child: SizedBox(
+        child: Align(
+            alignment: Alignment.bottomCenter,
+            child: ListView(children: <Widget>[
               ListTile(
                 title: Center(child: Text(appTitle, style: _textStyle)),
                 onTap: () {},
@@ -24,6 +24,6 @@ Drawer appbar(String appTitle) {
               _menuItem('Playlists', () {}),
               _menuItem('Cast', () {}),
               _menuItem('About', () {}),
-            ],
-          )));
+            ])),
+      ));
 }
